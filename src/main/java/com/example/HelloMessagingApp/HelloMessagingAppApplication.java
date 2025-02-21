01ffe561fadcb08a110e06c32c79e84094d268ef
Vidhi Jain <vidhijain507@gmail.com>
2025-02-21 13:39:16 +0530

    [Vidhi_Jain]Add. COde for Hello Messaging app.

---
+++ src/main/java/com/example/HelloMessagingApp/HelloMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloMessagingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMessagingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloMessagingApp/HelloMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloMessagingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
